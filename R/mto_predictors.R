234a1a947e2f94e78a004cbce874b89bc01ec9b6
dheimgartner <d.heimgartners@gmail.com>
2023-10-23 11:23:27 +0200

    implemented mto predictors
    (not wrapped in plumber yet)

---
+++ R/mto_predictors.R
@@ -0,0 +1,159 @@
+#' Base predictor
+#'
+#' @param data `data.frame` with data to predict probability of having GA subscription
+#' @param model Apollo model
+#' @param excl Coefficient vector to exclude (`c("c1", "c2", "scale")` will always be excluded)
+#' @param fix Named vector. Fix coefficients to certain values
+#' @param cc Calibration constant (cutoff parameter)
+#'
+#' @return `data.frame` with probability vectors
+#' @export
+base_predictor <- function(data, model, excl, fix = NULL, cc = NULL, return_vars = FALSE) {
+  est <- model$estimate
+  excl <- c(excl, "c1", "c2", "scale")
+
+  if (is.null(cc)) {
+    c1 <- est["c1"]
+  } else {
+    c1 <- cc
+  }
+
+  est <- est[names(est) %!in% excl]
+
+  if (!is.null(fix)) {
+    est[names(fix)] <- fix
+  }
+
+  v <- vars_from_beta(est)
+
+  if (return_vars) {
+    return(v)
+  }
+
+  db <-
+    data %>%
+    select(all_of(v)) %>%
+    as.matrix()
+
+  y_star <- -c1 + db %*% est
+  p1 <- pnorm(y_star)
+  p0 <- 1 - p1
+  p <- data.frame(p0 = p0, p1 = p1)
+  return(p)
+}
+
+
+
+
+#' GA predictor
+#'
+#' @param data `data.frame` with data to predict probability of having GA subscription
+#' @param model Apollo model
+#' @param fix Named vector. Fix coefficients to certain values
+#' @param cc Calibration constant (cutoff parameter)
+#'
+#' @return `data.frame` with probability vectors
+#'
+#' @seealso [base_predictor()]
+#' @export
+ga_predictor <- function(data, model, fix = NULL, cc = NULL, return_vars = FALSE) {
+  excl <- c("b_wfa", "b_pt_class_1", "b_wfh_x_wfa", "b_pt_fixed_cost", "b_pt_class_x_cost")
+  base_predictor(data = data, model = model, excl = excl, fix = fix, cc = cc, return_vars = return_vars)
+}
+
+
+
+
+#' Car predictor
+#'
+#' @param data `data.frame` with data to predict probability of owning a car
+#' @param model Apollo model
+#' @param fix Named vector. Fix coefficients to certain values
+#' @param cc Calibration constant (cutoff parameter)
+#'
+#' @return `data.frame` with probability vectors
+#'
+#' @seealso [base_predictor()]
+#' @export
+ca_predictor <- function(data, model, fix = NULL, cc = NULL, return_vars = FALSE) {
+  excl <- c("b_wfh_x_wfa", "b_ca_fixed_cost", "b_ca_type_1", "b_ca_type_2", "b_ca_type_3", "b_ca_type_4")
+  base_predictor(data = data, model = model, excl = excl, fix = fix, cc = cc, return_vars = return_vars)
+}
+
+
+
+
+#' Regional PT-subscription predictor
+#'
+#' @param data `data.frame` with data to predict probability of owning a regional subscription (PT)
+#' @param model Apollo model
+#' @param fix Named vector. Fix coefficients to certain values
+#' @param cc Calibration constant (cutoff parameter)
+#'
+#' @return `data.frame` with probability vectors
+#'
+#' @seealso [base_predictor()]
+#' @export
+re_predictor <- function(data, model, fix = NULL, cc = NULL, return_vars = FALSE) {
+  excl <- c("b_wfh_x_wfa")
+  base_predictor(data = data, model = model, excl = excl, fix = fix, cc = cc, return_vars = return_vars)
+}
+
+
+
+
+#' Half-fare card (HT) predictor
+#'
+#' @param data `data.frame` with data to predict probability of owning a HT
+#' @param model Apollo model
+#' @param fix Named vector. Fix coefficients to certain values
+#' @param cc Calibration constant (cutoff parameter)
+#'
+#' @return `data.frame` with probability vectors
+#'
+#' @seealso [base_predictor()]
+#' @export
+ht_predictor <- function(data, model, fix = NULL, cc = NULL, return_vars = FALSE) {
+  excl <- c("b_wfh_x_wfa")
+  base_predictor(data = data, model = model, excl = excl, fix = fix, cc = cc, return_vars = return_vars)
+}
+
+
+
+
+#' Car-sharing predictor
+#'
+#' @param data `data.frame` with data to predict probability of having a car-sharing subscription
+#' @param model Apollo model
+#' @param fix Named vector. Fix coefficients to certain values
+#' @param cc Calibration constant (cutoff parameter)
+#'
+#' @return `data.frame` with probability vectors
+#'
+#' @seealso [base_predictor()]
+#' @export
+cs_predictor <- function(data, model, fix = NULL, cc = NULL, return_vars = FALSE) {
+  excl <- c("b_wfh_x_wfa")
+  base_predictor(data = data, model = model, excl = excl, fix = fix, cc = cc, return_vars = return_vars)
+}
+
+
+
+
+#' Car-sharing predictor
+#'
+#' @param data `data.frame` with data to predict probability of having a car-sharing subscription
+#' @param model Apollo model
+#' @param fix Named vector. Fix coefficients to certain values
+#' @param cc Calibration constant (cutoff parameter)
+#'
+#' @return `data.frame` with probability vectors
+#'
+#' @seealso [base_predictor()]
+#' @export
+bi_predictor <- function(data, model, fix = NULL, cc = NULL, return_vars = FALSE) {
+  excl <- c("b_wfh_x_wfa")
+  base_predictor(data = data, model = model, excl = excl, fix = fix, cc = cc, return_vars = return_vars)
+}
+
+
--- inst/plumber/main/plumber.R
+++ inst/plumber/main/plumber.R
@@ -2,6 +2,9 @@
 #* @apiDescription Returns probability vectors for home office choice and frequency as well as mobility tool ownership.
 #* @apiContact Daniel Heimgartner `daniel.heimgartner@ivt.baug.ethz.ch`
 
+
+
+
 #* Echo back the input
 #* @param msg The message to echo
 #* @get /echo
@@ -10,10 +13,13 @@ function(msg="") {
   out
 }
 
-#* DCM for GA subscription
+
+
+
+#* DCM for GA (ga) subscription
 #* @post /predict/ga
 #* @serializer json
-#* @param data:data.frame Data to predict on (make sure to include the required variables) -> TODO: add get request
+#* @param data:data.frame Data to predict on (required vars: `/vars/ga`)
 #* @param fix:dbl Fix one or more coefficients
 #* @param cc:dbl Calibration constant
 #* @param debug:bool
@@ -24,7 +30,7 @@ function(data, fix = NULL, cc = NULL, debug = FALSE) {
   data <- as.data.frame(data)
   out <- ga_predictor(
     data = data,
-    model = MATSimAPI::MATSimAPI$ga$model,
+    model = MATSimAPI::mtomodels$ga$model,
     fix = fix,
     cc = cc,
     return_vars = FALSE
@@ -32,13 +38,13 @@ function(data, fix = NULL, cc = NULL, debug = FALSE) {
   out
 }
 
-#* Required variables for GA subscription
-#* @get /predict/ga/vars
+#* Required variables for GA (ga) model
+#* @get /vars/ga
 #* @serializer json
 function() {
   out <- ga_predictor(
     data = NULL,
-    model = MATSimAPI::MATSimAPI$ga$model,
+    model = MATSimAPI::mtomodels$ga$model,
     fix = NULL,
     cc = NULL,
     return_vars = TRUE
@@ -46,17 +52,46 @@ function() {
   out
 }
 
-#* Pass data in and out
-#* @post /test/data
-#* @param data:data.frame Data to pass in and out
+
+
+
+#* DCM for car (ca) ownership
+#* @post /predict/ca
+#* @serializer json
+#* @param data:data.frame Data to predict on (required vars: `/vars/ca`)
+#* @param fix:dbl Fix one or more coefficients
+#* @param cc:dbl Calibration constant
 #* @param debug:bool
-function(data, debug = FALSE) {
+function(data, fix = NULL, cc = NULL, debug = FALSE) {
   if (debug) {
     browser()
   }
-  df <- as.data.frame(data)
-  df[1, ]
-  return(df)
+  data <- as.data.frame(data)
+  out <- ca_predictor(
+    data = data,
+    model = MATSimAPI::mtomodels$ca$model,
+    fix = fix,
+    cc = cc,
+    return_vars = FALSE
+  )
+  out
 }
+
+#* Required variables for car (ca) model
+#* @get /vars/ca
+#* @serializer json
+function() {
+  out <- ca_predictor(
+    data = NULL,
+    model = MATSimAPI::mtomodels$ca$model,
+    fix = NULL,
+    cc = NULL,
+    return_vars = TRUE
+  )
+  out
+}
+
+
+
 
 
