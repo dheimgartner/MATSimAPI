df46b1b8439f71924d9bd394e578f8da1eaf1169
dheimgartner <d.heimgartners@gmail.com>
2023-10-25 11:57:03 +0200

    done with variables
    added imp (based on mean values from full modeling frame)

--- Makefile
+++ Makefile
@@ -1,5 +1,7 @@
 MTO_MODELS := ../mtomodel/data/MATSimAPI.rda
+MTO_FULL := ../mtomodel/data/full_db.rda
 WFH_MODELS := ../wfhmodel/data/MATSimAPI.rda
+WFH_FULL := ../wfhmodel/data/full_db.rda
 
 
 .PHONY: build unbuild update_mto_models update_wfh_models
@@ -16,10 +18,12 @@ unbuild:
 
 update_mto_models:
 	cp $(MTO_MODELS) ./data-raw/mtomodels.rda
+	cp $(MTO_FULL) ./data-raw/mtodata_full.rda
 	Rscript --vanilla ./data-raw/mtomodels.R
 	Rscript --vanilla ./data-raw/test_data.R
 
 update_wfh_models:
 	cp $(WFH_MODELS) ./data-raw/wfhmodels.rda
+	cp $(WFH_FULL) ./data-raw/wfhdata_full.rda
 	Rscript --vanilla ./data-raw/wfhmodels.R
 	Rscript --vanilla ./data-raw/test_data.R
--- data-raw/variables.R
+++ data-raw/variables.R
@@ -6,6 +6,22 @@ library(tidyverse)
 
 rm(list = ls())
 
+# Load full db for imputation values
+load("./data-raw/mtodata_full.rda")
+mto_full <- full_db
+
+load("./data-raw/wfhdata_full.rda")
+wfh_full <- full_db
+
+full_db <- append(mto_full, wfh_full)
+
+df_full <-
+  full_db %>%
+  reduce(bind_rows) %>%
+  group_by(ID) %>%
+  slice(1) %>%
+  ungroup()
+
 # Init
 dat <- test_data[names(test_data) %!in% "dummy"]
 
@@ -15,20 +31,22 @@ dat <-
     x$df
   })
 
-df <-
-  dat %>%
-  reduce(bind_rows)
-
 modes <- names(dat)[names(dat) != "wfh"]
 funcs <- list(ga_predictor, ca_predictor, re_predictor, ht_predictor, cs_predictor, bi_predictor)
 get_mto_vars <- function(mode, func) {
   func(data = NULL, model = mtomodels[[mode]]$model, return_vars = TRUE)
 }
 
-all_vars <-
+mto_vars <-
   map2(modes, funcs, function(x, y) {
     get_mto_vars(x, y)
   })
+
+wfh_vars <-
+  wfh_predictor(NULL, wfhmodels$heckman, return_vars = TRUE)
+
+all_vars <- mto_vars
+all_vars$wfh <- wfh_vars
 
 all_vars <-
   all_vars %>%
@@ -37,11 +55,26 @@ all_vars <-
 
 all_vars <- c("ID", all_vars) # ID is always required
 
-df <- df[, all_vars]
+df <- df_full[, all_vars]
+
+# Compute mean values for all (even for dummies -> market share)
+sel <- all_vars[all_vars != "ID"]
+imp <-
+  df %>%
+  select(all_of(sel)) %>%
+  map_vec(function(x) {
+    mean(x, na.rm = TRUE)
+  }) %>%
+  sort()
+
+df_imp <-
+  data.frame(imp) %>%
+  rownames_to_column("variable")
 
 variables <-
   data.frame(variable = all_vars, description = NA, type = NA, unit = NA) %>%
-  arrange(variable)
+  arrange(variable) %>%
+  left_join(df_imp, by = "variable")
 
 # Manual labour
 path <- "./data-raw/variables.xlsx"
