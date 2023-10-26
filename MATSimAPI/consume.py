91330a2efe75be850277f2b8a8b648d41ab3793b
dheimgartner <d.heimgartners@gmail.com>
2023-10-26 12:55:30 +0200

    black

--- MATSimAPI/consume.py
+++ MATSimAPI/consume.py
@@ -29,23 +29,27 @@ class RApiConsumer:
         if self.api_process:
             self.api_process.terminate()
             self.api_process.wait()
-            
+
     @classmethod
     def _stop_api_by_port(cls, port):
-        for proc in psutil.process_iter(attrs=['pid', 'name', 'connections']):
+        for proc in psutil.process_iter(attrs=["pid", "name", "connections"]):
             try:
-                if proc.info.get('connections') is not None:
-                    for conn in proc.info['connections']:
-                        if conn.laddr.port == port and proc.info['name'] == 'R':
-                            print(f"Found process {proc.info['name']} with PID {proc.info['pid']} using port {port}. Terminating...")
-                            psutil.Process(proc.info['pid']).terminate()
+                if proc.info.get("connections") is not None:
+                    for conn in proc.info["connections"]:
+                        if conn.laddr.port == port and proc.info["name"] == "R":
+                            print(
+                                f"Found process {proc.info['name']} with PID {proc.info['pid']} using port {port}. Terminating..."
+                            )
+                            psutil.Process(proc.info["pid"]).terminate()
             except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
                 pass
-                    
+
     def wait_for_api(self, max_retries=10, retry_interval=2):
         for _ in range(max_retries):
             try:
-                response = requests.get(f"http://localhost:{self.port}/echo?msg=wait_for_api") # without /echo 404 is returned -> echo to check connection
+                response = requests.get(
+                    f"http://localhost:{self.port}/echo?msg=wait_for_api"
+                )  # without /echo 404 is returned -> echo to check connection
                 if response.status_code == 200:
                     return
             except requests.exceptions.RequestException:
@@ -64,7 +68,7 @@ class RApiConsumer:
 
 if __name__ == "__main__":
     url = "http://localhost:8000"
-    
+
     with RApiConsumer(port=8000) as api:
         try:
             response = requests.get(f"{url}/doc?model=all")
--- MATSimAPI/entry_point.py
+++ MATSimAPI/entry_point.py
@@ -17,34 +17,38 @@ import argparse
 from . import install
 from .consume import RApiConsumer
 
+
 def create_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(
-        prog='MATSimAPI',
-        description='Entry point to interact with the API'
+        prog="MATSimAPI", description="Entry point to interact with the API"
     )
-    
-    parser.add_argument('cmd', help='Command to run {install|uninstall|start|stop}')
-    parser.add_argument('--port', type=int, default=8000, help='Port where the API should run')
-    
+
+    parser.add_argument("cmd", help="Command to run {install|uninstall|start|stop}")
+    parser.add_argument(
+        "--port", type=int, default=8000, help="Port where the API should run"
+    )
+
     return parser
+
 
 def run() -> None:
     p = create_parser()
     a = p.parse_args()
-    
+
     api = RApiConsumer(port=a.port)
-    
-    if a.cmd == 'install':
+
+    if a.cmd == "install":
         install.install_r_package()
-    elif a.cmd == 'uninstall':
+    elif a.cmd == "uninstall":
         install.uninstall_r_package()
-    elif a.cmd == 'start':
+    elif a.cmd == "start":
         api.start_api()
         api.wait_for_api()
-    elif a.cmd == 'stop':
+    elif a.cmd == "stop":
         api._stop_api_by_port(port=a.port)
     else:
         p.print_help()
-    
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     run()
