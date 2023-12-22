5d983b159331ba7f17047c0a1a2d274e2e12f0cf
dheimgartner <d.heimgartners@gmail.com>
2023-12-22 18:17:39 +0100

    Updated with calibrated model

--- MATSimAPI/Rapi/data-raw/wfhmodels.R
+++ MATSimAPI/Rapi/data-raw/wfhmodels.R
@@ -3,7 +3,8 @@
 # see Makefile, rule update_wfh_modles
 
 load("./data-raw/wfhmodels.rda")
-wfhmodels <- list(heckman = MATSimAPI$heckman)
+wfhmodels <- list(heckman_base = MATSimAPI$heckman,  # as estimated (not calibrated)
+                  heckman = MATSimAPI$calibrated)  # calibrated to synpop zurich
 
 
 usethis::use_data(wfhmodels, overwrite = TRUE)
